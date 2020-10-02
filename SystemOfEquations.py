82ce038c2a1827c0a80efafafa03fcb98c6a4e32
Benjamin Schnapp <bdschnapp@gmail.com>
2020-10-02 12:11:04 -0400

    Added gauss_seidel_relaxed method

--- SystemOfEquations.py
+++ SystemOfEquations.py
@@ -72,3 +72,41 @@ class SystemOfLinearEquations:
                 return x
         # Returns x once max iterations is reached, x will contain approximate values
         return x
+
+    def gauss_seidel_relaxed(self, max_iterations, guess=1, omega=1.5):
+        # Pivots rows so that matrix[i][i] is never 0
+        repeat = False
+        count = 0
+        while repeat:
+            repeat = False
+            for i in range(self.size):
+                if self.matrix[i][i] == 0:
+                    self.swap(i)
+                repeat = True
+            if count > self.size:
+                return False
+            count += 1
+
+        # Initializes vectors for tracing iteration results and checking if its the final answer
+        x = [guess for i in range(self.size)]
+        correct = [False for i in range(self.size)]
+
+        # Loops through up to the max iteration number, or until the correct result is found
+        for k in range(max_iterations):
+            for i in range(self.size):
+                xn = 0
+                for j in range(self.size):
+                    if i != j:
+                        xn = xn + self.matrix[i][j] * x[j]
+                x[i] = (omega * (self.matrix[i][self.size + 1] - xn) / self.matrix[i][i]) + ((1 - omega) * guess)
+
+                # Checks if the current x value for the current iteration is correct
+                if(xn + (self.matrix[i][i] * x[i])) == self.matrix[i][self.size + 1]:
+                    correct[i] = True
+
+            # Checks if all x values for the current iteration are correct
+            if not(False in correct):
+                # Returns x before max iterations is reached if all values are correct
+                return x
+        # Returns x once max iterations is reached, x will contain approximate values
+        return x
